23961968cc0089ce982cad18d954dc1c7c34cfdc
Your Name <you@example.com>
2024-02-27 00:35:27 +0600

    fouthd-commit

--- app/(home)/_components/nabbar/navbar.tsx
+++ app/(home)/_components/nabbar/navbar.tsx
@@ -5,7 +5,16 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 import DropDownMenu from './drop-down-menu';
 
-const Navbar = () => {
+
+interface NavbarProps {
+    scrollToWebsiteDesign: ()=> void;
+    scrollToGraphicDesign: ()=> void;
+    scrollToshopifyStoresDesign: ()=> void;
+    scrollToBrandsDesing: ()=> void;
+}
+
+
+const Navbar:React.FC<NavbarProps> = ({scrollToWebsiteDesign, scrollToGraphicDesign, scrollToshopifyStoresDesign, scrollToBrandsDesing}) => {
     const [isDropDownVisible, setDropDownVisible] = useState(false);
 
     const toggleDropDown = () => {
@@ -35,10 +44,10 @@ const Navbar = () => {
 
                 <div className='cursor-pointer hidden lg:flex space-x-10 items-center text-slate-300 text-center 
                 bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 bg-neutral-400 bg-opacity-50'>
-                    <div className='hover:text-gray-50'>Website Design</div>
-                    <div className='hover:text-gray-50'>Graphic Design</div>
-                    <div className='hover:text-gray-50'>Shopity Design</div>
-                    <div className='hover:text-gray-50'> Brands</div>
+                    <div onClick={scrollToWebsiteDesign} className='hover:text-gray-50'>Website Design</div>
+                    <div onClick={scrollToGraphicDesign} className='hover:text-gray-50'>Graphic Design</div>
+                    <div onClick={scrollToshopifyStoresDesign} className='hover:text-gray-50'>Shopity Design</div>
+                    <div onClick={scrollToBrandsDesing} className='hover:text-gray-50'> Brands</div>
                     <Link href={'/pricing'} className='hover:text-gray-50'>
                         Pricing
                     </Link>
@@ -46,10 +55,10 @@ const Navbar = () => {
 
                 <div className='hidden md:flex'>
                     <Link href={"/contact"} className='inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'>
-                      Contact
+                        Contact
                     </Link>
                 </div>
-                
+
                 <div className='flex md:hidden'>
                     {isDropDownVisible ? (
                         <div
@@ -73,4 +82,6 @@ const Navbar = () => {
     )
 }
 
-export default Navbar
+export default Navbar;
+
+
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Navbar from "@/app/(home)/_components/nabbar/navbar";
 import { Spotlight } from "@/components/ui/Spotlight";
 import Link from "next/link";
@@ -8,35 +9,78 @@ import ShopifyStores from "./_components/shopify-stores";
 import Brands from "./_components/brands";
 import Services from "./_components/services";
 import Fiq from "./_components/faq";
+import { useRef } from "react";
+
 
 export default function Home() {
   
+  const websiteDesignRef = useRef<HTMLDivElement>(null);
+  const graphicDesignRef = useRef<HTMLDivElement>(null);
+  const shopifyStoresRef = useRef<HTMLDivElement>(null);
+  const brandsRef = useRef<HTMLDivElement>(null);
+
+
+  const scrollToWebsiteDesign = ()=>{
+    websiteDesignRef.current?.scrollIntoView({behavior: "smooth"})
+  }
+  
+  const scrollToGraphicDesign = ()=>{
+    graphicDesignRef.current?.scrollIntoView({behavior: "smooth"})
+  }
+
+  const scrollToshopifyStoresDesign = ()=>{
+    shopifyStoresRef.current?.scrollIntoView({behavior: "smooth"})
+  }
+
+  const scrollToBrandsDesing = ()=>{
+    brandsRef.current?.scrollIntoView({behavior: "smooth"})
+  }
+
+
+
   return (
     <div className="w-full md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.2] relative overflow-hidden">
-      <Navbar/>
-      <Spotlight className="left-60 hidden md:flex" fill="white" />
+      <Navbar
+      scrollToWebsiteDesign={scrollToWebsiteDesign}
+      scrollToGraphicDesign={scrollToGraphicDesign}
+      scrollToshopifyStoresDesign={scrollToshopifyStoresDesign}
+      scrollToBrandsDesing={scrollToBrandsDesing}
+      />
+      <Spotlight className="left-80 top-[-900px] hidden md:flex" fill="white" />
 
       <div className="p-4 mx-auto relative z-10 w-full pt-10 md:pt-24 px-2">
         <div className="text-4xl pb-5 md:text-7xl px-5 text-center bg-clip-text text-transparent  bg-gradient-to-b from-neutral-50  bg-neutral-400 bg-opacity-50">
-          Create, grow, and<br/> scale your business
+          Create, grow, and<br /> scale your business
         </div>
         <p className="mt-4 text-lg font-normal text-neutral-300 max-w-lg text-center mx-auto px-4">
           Custom tailored solutions for your business,we are a team of creatives who excited to help you grow your business.
         </p>
         <Link href={"/book"}
-        className="cursor-pointer flex items-center justify-center border rounded-full w-48 p-2 mx-auto my-6 text-white ">
+          className="cursor-pointer flex items-center justify-center border rounded-full w-48 p-2 mx-auto my-6 text-white ">
           Book a call
         </Link>
 
         <div className="w-full pt-20">
-          <SliderOne/>
-          <WebsiteDesign/>
-          <GraphicDesign/>
-          <ShopifyStores/>
-          <Brands/>
-          <Services />
-          <Fiq/>
-        
+          <SliderOne />
+          <div ref={websiteDesignRef}>
+            <WebsiteDesign />
+          </div>
+          <div ref={graphicDesignRef}>
+            <GraphicDesign />
+          </div>
+          <div ref={shopifyStoresRef} >
+            <ShopifyStores />
+          </div>
+          <div ref={brandsRef} >
+            <Brands />
+            <Services />
+            <Fiq />
+           
+          </div>
+
+
+
+
         </div>
 
 
